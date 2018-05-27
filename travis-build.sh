cee9a564fde215a9e1604f27b5eb90e43f762bd7
Istvan Miklos <imiklos2@inf.u-szeged.hu>
2018-05-27 22:23:07 +0200

    Travis auto deploy to heroku

--- travis-build.sh
+++ travis-build.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./gradlew clean test assemble
-
-filename=$(find build/libs -name "*.jar" | head -1)
-filename=$(basename "$filename")
-
-EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-
-  echo "Publishing archives"
-
-  if [[ -n $TRAVIS_TAG ]]; then
-      ./gradlew publish || EXIT_STATUS=$?
-  else
-      EXIT_STATUS=$?
-  fi
-fi
-
-exit $EXIT_STATUS
+./gradlew -Dgrails.env=prod clean build
