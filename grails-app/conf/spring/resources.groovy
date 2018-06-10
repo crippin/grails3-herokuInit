058c2d2c17942576bd2038813d774105f7e8fbae
Istvan Miklos <imiklos2@inf.u-szeged.hu>
2018-06-10 22:35:46 +0200

    init SpringSecurityCore

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import grails3.herokuinit.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
--- grails-app/init/grails3/herokuinit/BootStrap.groovy
+++ grails-app/init/grails3/herokuinit/BootStrap.groovy
@@ -3,6 +3,18 @@ package grails3.herokuinit
 class BootStrap {
 
     def init = { servletContext ->
+        def authorities = ['ROLE_USER']
+        authorities.each {
+            if ( !Role.findByAuthority(it) ) {
+                new Role(authority: it).save()
+            }
+        }
+        if ( !User.findByUsername('sherlock') ) {
+            def u = new User(username: 'egy', password: 'almafa')
+            u.save()
+            def ur = new UserRole(user: u, role:  Role.findByAuthority('ROLE_USER'))
+            ur.save()
+        }
     }
     def destroy = {
     }
